dce85391142f0c73a855f4309c8bbbe0efa8606c
hudsonbay <manuelmenendezalfonso@gmail.com>
2022-02-12 16:58:58 -0500

    test: :sparkles: Added new tests

--- test/quickcheck_playground_test.exs
+++ test/quickcheck_playground_test.exs
@@ -1,8 +1,37 @@
 defmodule QuickcheckPlaygroundTest do
   use ExUnit.Case
+  use EQC.ExUnit
+  import QuickcheckPlayground
+
   doctest QuickcheckPlayground
 
-  test "greets the world" do
-    assert QuickcheckPlayground.hello() == :world
+  property "reversing a list twice yields the original list" do
+    forall l <- list(int) do
+      ensure(l |> Enum.reverse() |> Enum.reverse() == l)
+    end
+  end
+
+  property "encoding is the reverse of decoding" do
+    forall bin <- binary do
+      ensure(bin |> Base.encode64() |> Base.decode64!() == bin)
+    end
+  end
+
+  property "sorting works" do
+    forall l <- list(int) do
+      ensure(l |> Enum.sort() |> is_sorted == true)
+    end
+  end
+
+  property "appending an element and sorting it is the same as prepending an element and sorting it" do
+    forall {i, l} <- {int, list(int)} do
+      [i | l] |> Enum.sort() == (l ++ [i]) |> Enum.sort()
+    end
+  end
+
+  property "calling Enum.uniq/1 twice has no effect" do
+    forall l <- list(int) do
+      ensure(l |> Enum.uniq() == l |> Enum.uniq() |> Enum.uniq())
+    end
   end
 end
